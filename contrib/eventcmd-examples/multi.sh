f2f324ffa8b8466f36c250bc49fd5b8f6712ce8b
Joshua V. Dillon <jvdillon@gatech.edu>
2012-09-22 08:58:39 -0700

    Adding missing source files.

---
+++ contrib/eventcmd-examples/multi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Executes all scripts located in ~/.config/pianobarfly/eventcmd.d/ as if they
+# were called by pianobarfly directly
+
+STDIN=`mktemp`
+cat >> $STDIN
+
+for F in ~/.config/pianobarfly/eventcmd.d/*; do
+	if [ -x "$F" ]; then
+		"$F" $@ < "$STDIN"
+	fi
+done
+
+rm "$STDIN"
+
